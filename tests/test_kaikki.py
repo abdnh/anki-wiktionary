639dfc211db3f904f268e8850c81ca3e13264ee5
Abdo <abd.nh25@gmail.com>
2022-08-31 00:16:46 +0300

    Fix Kaikki tests

--- tests/test_kaikki.py
+++ tests/test_kaikki.py
@@ -2,10 +2,10 @@ import builtins
 import tempfile
 import unittest
 from pathlib import Path
-from typing import Any, Callable, List, Union
+from typing import Any, Union
 from unittest.mock import patch
 
-from src.wiktionary_fetcher import WiktionaryFetcher
+from src.dictionaries.kaikki import GeneralParser, KaikkiDict
 
 
 def mock_open(file: Union[str, Path], *args: Any, **kwargs: Any) -> Any:
@@ -15,11 +15,11 @@ def mock_open(file: Union[str, Path], *args: Any, **kwargs: Any) -> Any:
     return builtins.open(file, *args, **kwargs)  # pylint: disable=unspecified-encoding
 
 
-class TestWiktionaryFetcher(unittest.TestCase):
+class TestKaikkiDict(unittest.TestCase):
     DICT_NAME = "dict"
 
     def test_importing(self) -> None:
-        patcher = patch("src.wiktionary_fetcher.open", side_effect=mock_open)
+        patcher = patch("src.dictionaries.kaikki.open", side_effect=mock_open)
         patcher.start()
         failed_words = []
 
@@ -30,34 +30,24 @@ class TestWiktionaryFetcher(unittest.TestCase):
         tests_dir = Path(__file__).parent
         with tempfile.TemporaryDirectory() as tmp_dir_s:
             tmp_dir = Path(tmp_dir_s)
-            count = WiktionaryFetcher.dump_kaikki_dict(
+            count = KaikkiDict.build_dict(
                 tests_dir / "test_dict.json",
-                self.DICT_NAME,
+                tmp_dir / self.DICT_NAME,
                 on_progress=lambda _: True,
                 on_error=on_error,
-                base_dir=tmp_dir,
             )
             assert count == 2
             assert len(failed_words) == 1
             assert failed_words[0] == "FAIL"
             patcher.stop()
-            fetcher = WiktionaryFetcher(self.DICT_NAME, base_dir=tmp_dir)
-            assert fetcher.get_gender("кошка") == "feminine"
-            assert fetcher.get_senses("кошка")[0] == "cat"
-            assert fetcher.get_part_of_speech("кошка") == "noun"
+            fetcher = KaikkiDict(tmp_dir / self.DICT_NAME)
+            parser = GeneralParser()
+            entry = fetcher.lookup("кошка", parser)
+            assert entry.gender == "feminine"
+            assert entry.definitions[0] == "cat"
+            assert entry.pos == "noun"
             assert (
-                fetcher.get_examples("кошка")[0]
+                entry.examples[0]
                 == "жить как ко́шка с соба́кой / to lead a cat-and-dog life"
             )
-            methods: List[Callable[[str], Any]] = [
-                fetcher.get_examples,
-                fetcher.get_gender,
-                fetcher.get_part_of_speech,
-                fetcher.get_senses,
-            ]
-            for method in methods:
-                try:
-                    method("FAIL")
-                    assert False
-                except:
-                    assert True
+            assert fetcher.lookup("FAIL", parser) is None
